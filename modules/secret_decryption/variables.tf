1f5d1c76c0b22179c4df5b40d6c2f1aff1badfd1
patoarvizu <patoarvizu@gmail.com>
2018-08-20 11:33:18 -0400

    Add 'secret_decryption' module

---
+++ modules/secret_decryption/variables.tf
@@ -0,0 +1,10 @@
+variable "encrypted_secret" {
+  type = "string"
+  description = "KMS-encrypted secret to be decrypted"
+}
+
+variable "secret_context" {
+  type = "map"
+  description = "Encryption context associated with the secret"
+  default = {}
+}
