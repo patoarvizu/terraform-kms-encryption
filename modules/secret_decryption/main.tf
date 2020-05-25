2df7ace1dc5ae6edb41a6e83f962eae2b4ffab36
patoarvizu <patoarvizu@gmail.com>
2020-05-24 20:32:47 -0400

    Update 'secret_decryption' module to 0.12

--- modules/secret_decryption/main.tf
+++ modules/secret_decryption/main.tf
@@ -1,7 +1,7 @@
 data "aws_kms_secrets" "secret" {
   secret {
     name    = "decrypted_secret"
-    payload = "${var.encrypted_secret}"
-    context = "${var.secret_context}"
+    payload = var.encrypted_secret
+    context = var.secret_context
   }
 }
