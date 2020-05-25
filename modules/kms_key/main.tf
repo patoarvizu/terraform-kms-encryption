35f5a14b309147510192b852bf9f4f693f3a86af
patoarvizu <patoarvizu@gmail.com>
2020-05-24 20:03:16 -0400

    Update 'kms_key' module to 0.12

--- modules/kms_key/main.tf
+++ modules/kms_key/main.tf
@@ -1,8 +1,8 @@
 resource "aws_kms_key" "key" {
-  policy = "${data.aws_iam_policy_document.policy.json}"
+  policy = data.aws_iam_policy_document.policy.json
 }
 
 resource "aws_kms_alias" "alias" {
   name          = "alias/${var.alias_name}"
-  target_key_id = "${aws_kms_key.key.key_id}"
+  target_key_id = aws_kms_key.key.key_id
 }
